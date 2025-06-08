c6fa1f8ec662b4a189139af7fe07a4acc5ae0018
katherine4G <112660945+katherine4G@users.noreply.github.com>
2025-06-07 18:29:05 -0600

    rep

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({ dest: "public",
+register: true, skipWaiting: true,
+disable: process.env.NODE_ENV === "development"
+});
+module.exports = withPWA({ reactStrictMode: true });
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// import type { NextConfig } from "next";
 
-export default nextConfig;
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
